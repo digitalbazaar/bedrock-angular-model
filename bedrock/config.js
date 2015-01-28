01c4728743ba127df2aaae83d8a5582e8e1cf386
Dave Longley <dlongley@digitalbazaar.com>
2015-01-28 17:52:47 -0500

    Add bedrock.json and tests.

---
+++ bedrock/config.js
@@ -0,0 +1,15 @@
+/*
+ * Bedrock Configuration.
+ *
+ * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
+ */
+var path = require('path');
+
+module.exports = function(bedrock) {
+  if(bedrock.config.protractor) {
+    var config = bedrock.config.protractor.config;
+    // add protractor tests
+    config.suites['bedrock-angular-model'] =
+      path.join(__dirname, './tests/**/*.js');
+  }
+};
